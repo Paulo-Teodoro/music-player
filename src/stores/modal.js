24c61a6311ca2f3c8a55f25ffd068793eede049c
Paulo Teodoro <paulolimrs@outlook.com>
2022-07-28 22:14:20 -0300

    feat: show or hidden auth modal

---
+++ src/stores/modal.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+export const useModalStore = defineStore("modal", {
+  state: () => {
+    return { isOpen: false };
+  },
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? "hidden" : "";
+    },
+  },
+});
