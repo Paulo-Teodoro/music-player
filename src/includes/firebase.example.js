a5edbc830f72256ab7ca9907a05134788abb953e
Paulo Teodoro <paulolimrs@outlook.com>
2022-10-20 23:20:16 -0300

    feat: install firebase

---
+++ src/includes/firebase.example.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
